137d9b84ab5a9a04c02a7f23432a1aa7ff337526
Louie Schoenknecht <luisesch97@gmail.com>
2021-04-14 10:30:30 -0700

    write code to count number of vowels in a word
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- src/haiku-checker.js
+++ src/haiku-checker.js
@@ -2,8 +2,12 @@
 
 // }
 
-export function countVowels() {
-
+export function countVowels(string) {
+  if (string.match(/[aeiouy]/gi)) {
+    return string.match(/[aeiouy]/gi).length;
+  } else {
+    return "you ain't got no vowels my friend";
+  }
 }
 
 // countDips()
